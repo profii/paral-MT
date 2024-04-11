25e34eb495a586145ebe3565c310c455978b3ae2
Anastasiia Demidova <anastasiademidov@mail.ru>
2024-04-11 11:33:24 +0400

    Add K8s files

---
+++ run.sh
@@ -0,0 +1,6 @@
+pip install torchtext==0.5.0 nltk==3.5 wandb # deepspeed
+pip install -U spacy==3.7.4
+pip install https://github.com/explosion/spacy-models/releases/download/en_core_web_lg-3.7.0/en_core_web_lg-3.7.0.tar.gz
+pip install https://github.com/explosion/spacy-models/releases/download/fr_core_news_lg-3.7.0/fr_core_news_lg-3.7.0.tar.gz
+
+python basic.py
